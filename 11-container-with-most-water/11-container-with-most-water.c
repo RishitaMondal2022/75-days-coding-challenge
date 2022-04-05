637dc4029962f5a776eff0e1030e203a5394745f
Rishita Mondal <102499315+RishitaMondal2022@users.noreply.github.com>
2022-04-05 10:42:34 +0530

    Time: 227 ms (8.41%), Space: 11.7 MB (56.14%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.c
@@ -0,0 +1,13 @@
+
+
+int maxArea(int* height, int heightSize){
+    int water = 0, *i = height, *j = i + heightSize - 1;
+    while (i < j) {
+        int h = *i < *j ? *i : *j;
+        int w = (j - i) * h;
+        if (w > water) water = w;
+        while (*i <= h && i < j) i++;
+        while (*j <= h && i < j) j--;
+    }
+    return water;
+}
